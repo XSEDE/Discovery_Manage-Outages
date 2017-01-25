5f73f1b9e2fcccb10419440abe7005cb32127cfa
blau <blau@1370f987-b873-4e1f-8c66-8b3e5403fd71>
2017-01-25 17:17:05 +0000

    location configuration changes for new xci production layout

--- xci/source/info.apps/Manage-Outages/trunk/sbin/route_outages.sh
+++ xci/source/info.apps/Manage-Outages/trunk/sbin/route_outages.sh
@@ -4,10 +4,11 @@
 PYTHON=/soft/python-2.7.11/bin/python
 export PYTHON
 export LD_LIBRARY_PATH=/soft/python-2.7.11/lib
-export PYTHONPATH=/soft/warehouse-1.0/PROD/apps/:/soft/warehouse-1.0/PROD/django_xsede_warehouse
+#export PYTHONPATH=/soft/warehouse-1.0/PROD/apps/:/soft/warehouse-1.0/PROD/django_xsede_warehouse
+export PYTHONPATH=/soft/warehouse-1.0/PROD/django_xsede_warehouse
 export DJANGO_CONF=/soft/warehouse-1.0/conf/settings_info_mgmt.conf
 export DJANGO_SETTINGS_MODULE=xsede_warehouse.settings
 #python ./serialize_save.py
 #python ./route_speedpage.py ${@:1}
 #python ./speedserialize ${@:1}
-$PYTHON ./routeoutages.py
+$PYTHON /soft/warehouse-apps-1.0/Manage-Outages/PROD/sbin/routeoutages.py
