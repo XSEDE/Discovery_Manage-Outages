e5939a632a599b76211f64c9ccbffb233602896b
blau <blau@anl.gov>
2017-01-25 15:58:05 +0000

    rename RouteOutages for consistency

---
+++ sbin/routeoutages.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import pprint
+import os
+import pwd
+import re
+import sys
+import argparse
+import logging
+import logging.handlers
+import signal
+import datetime
+from datetime import datetime, tzinfo, timedelta
+from time import sleep
+import httplib
+import json
+import csv
+import ssl
+import shutil
+
+import django
+django.setup()
+from django.utils.dateparse import parse_datetime
+from outages.models import *
+from django.core import serializers
+
+#default_file = '/Users/blau/endpoint_descriptions/trunk/apps/RouteOutages/outages.csv'
+default_file = '/soft/warehouse-1.0/PROD/apps/RouteOutages/allOutageReport.csv'
+#snarfing the whole database is not the way to do it, for this anyway)
+#databasestate = serializers.serialize("json", ProjectResource.objects.all())
+with open(default_file, 'r') as my_file:
+    tgcdb_csv = csv.DictReader(my_file)
+    for row in tgcdb_csv:
+    #    if len(row['Content'])>1023:
+    #        print len(row['Content'])
+        #InDBAlready = ProjectResource.objects.filter(**row)
+        InDBAlready = Outages.objects.filter(OutageID=row['OutageID'])
+        if not InDBAlready:
+            objtoserialize={}
+            objtoserialize["model"]="outages.Outages"
+            objtoserialize["pk"]=row['OutageID']
+            objtoserialize["fields"]=row
+            jsonobj = json.dumps([objtoserialize])
+            moduleobjects =serializers.deserialize("json", jsonobj)
+
+            for obj in moduleobjects:
+                obj.save()
