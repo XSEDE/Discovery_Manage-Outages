ef0c8b42ef5448033ab0dda715d67b73cba0aa1f
JP Navarro <jpfnavarro@gmail.com>
2017-10-12 15:22:58 +0000

    Move tag

--- sbin/route_outages.sh
+++ sbin/route_outages.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
+MY_ROOT=/soft/warehouse-apps-1.0/Manage-Outages/PROD
+WAREHOUSE_ROOT=/soft/warehouse-1.0/PROD
+PYTHON=/soft/python-current/bin/python
+export LD_LIBRARY_PATH=/soft/python-current/lib/
+
 #export DJANGO_CONF=/Users/blau/info_services/info.warehouse/trunk/django_xsede_warehouse/xsede_warehouse/settings_localdev.conf
 #export PYTHONPATH=/Users/blau/info_services/info.warehouse/trunk/apps:/Users/blau/info_services/info.warehouse/trunk/django_xsede_warehouse
-PYTHON=/soft/python-2.7.11/bin/python
-export PYTHON
-export LD_LIBRARY_PATH=/soft/python-2.7.11/lib
-#export PYTHONPATH=/soft/warehouse-1.0/PROD/apps/:/soft/warehouse-1.0/PROD/django_xsede_warehouse
-export PYTHONPATH=/soft/warehouse-1.0/PROD/django_xsede_warehouse
+export PYTHONPATH=${WAREHOUSE_ROOT}/django_xsede_warehouse
 export DJANGO_CONF=/soft/warehouse-1.0/conf/settings_info_mgmt.conf
 export DJANGO_SETTINGS_MODULE=xsede_warehouse.settings
-#python ./serialize_save.py
-#python ./route_speedpage.py ${@:1}
-#python ./speedserialize ${@:1}
-$PYTHON /soft/warehouse-apps-1.0/Manage-Outages/PROD/sbin/routeoutages.py
+
+${PYTHON} ${MY_ROOT}/sbin/routeoutages.py
