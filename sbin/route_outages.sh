0879b272380ae63a2cf75d0e7da14ba698ce9cc7
JP Navarro <jpfnavarro@gmail.com>
2018-02-09 12:51:46 +0000

    Fix to point to renamed config

--- sbin/route_outages.sh
+++ sbin/route_outages.sh
@@ -7,7 +7,7 @@ export LD_LIBRARY_PATH=/soft/python-current/lib/
 #export DJANGO_CONF=/Users/blau/info_services/info.warehouse/trunk/django_xsede_warehouse/xsede_warehouse/settings_localdev.conf
 #export PYTHONPATH=/Users/blau/info_services/info.warehouse/trunk/apps:/Users/blau/info_services/info.warehouse/trunk/django_xsede_warehouse
 export PYTHONPATH=${WAREHOUSE_ROOT}/django_xsede_warehouse
-export DJANGO_CONF=/soft/warehouse-1.0/conf/settings_info_mgmt.conf
+export DJANGO_CONF=/soft/warehouse-1.0/conf/django_prod_mgmt.conf
 export DJANGO_SETTINGS_MODULE=xsede_warehouse.settings
 
 ${PYTHON} ${MY_ROOT}/sbin/routeoutages.py
