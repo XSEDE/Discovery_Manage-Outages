fad9ce41545d92fa8ba5e22f84abbeb9d93bf696
JP Navarro <jpfnavarro@gmail.com>
2018-09-11 00:28:59 +0000

    Python 3.6 w/ pipenv upgrade

--- sbin/route_outages.sh
+++ sbin/route_outages.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 MY_ROOT=/soft/warehouse-apps-1.0/Manage-Outages/PROD
 WAREHOUSE_ROOT=/soft/warehouse-1.0/PROD
-PYTHON=/soft/python-current/bin/python
-export LD_LIBRARY_PATH=/soft/python-current/lib/
+
+PYTHON_BASE=/soft/python/python-3.6.6-base
+export LD_LIBRARY_PATH=${PYTHON_BASE}/lib
+
+PYTHON_ROOT=/soft/warehouse-apps-1.0/python
+source ${PYTHON_ROOT}/bin/activate
 
 #export DJANGO_CONF=/Users/blau/info_services/info.warehouse/trunk/django_xsede_warehouse/xsede_warehouse/settings_localdev.conf
 #export PYTHONPATH=/Users/blau/info_services/info.warehouse/trunk/apps:/Users/blau/info_services/info.warehouse/trunk/django_xsede_warehouse
@@ -10,4 +14,4 @@ export PYTHONPATH=${WAREHOUSE_ROOT}/django_xsede_warehouse
 export DJANGO_CONF=/soft/warehouse-1.0/conf/django_prod_mgmt.conf
 export DJANGO_SETTINGS_MODULE=xsede_warehouse.settings
 
-${PYTHON} ${MY_ROOT}/sbin/routeoutages.py
+python ${MY_ROOT}/sbin/routeoutages.py
