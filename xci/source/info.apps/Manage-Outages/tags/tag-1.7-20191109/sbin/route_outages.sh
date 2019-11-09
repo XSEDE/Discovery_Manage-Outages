f09f136bed2e6b58fb1a8c85e439aa6b913df5e9
navarro <navarro@1370f987-b873-4e1f-8c66-8b3e5403fd71>
2019-11-09 22:52:30 +0000

    For production

---
+++ xci/source/info.apps/Manage-Outages/tags/tag-1.7-20191109/sbin/route_outages.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+MY_ROOT=/soft/warehouse-apps-1.0/Manage-Outages/PROD
+WAREHOUSE_ROOT=/soft/warehouse-1.0/PROD
+
+PYTHON_BASE=/soft/python/python-3.6.6-base
+export LD_LIBRARY_PATH=${PYTHON_BASE}/lib
+
+PYTHON_ROOT=/soft/warehouse-apps-1.0/Manage-Outages/python
+source ${PYTHON_ROOT}/bin/activate
+
+#export DJANGO_CONF=/Users/blau/info_services/info.warehouse/trunk/django_xsede_warehouse/xsede_warehouse/settings_localdev.conf
+#export PYTHONPATH=/Users/blau/info_services/info.warehouse/trunk/apps:/Users/blau/info_services/info.warehouse/trunk/django_xsede_warehouse
+export PYTHONPATH=${WAREHOUSE_ROOT}/django_xsede_warehouse
+export DJANGO_CONF=/soft/warehouse-apps-1.0/Manage-Outages/conf/django_xsede_warehouse.conf
+export DJANGO_SETTINGS_MODULE=xsede_warehouse.settings
+
+python ${MY_ROOT}/sbin/route_outages.py
