3f49eb2a55065660cf13a94b0900a5b01f3c88b8
navarro <navarro@1370f987-b873-4e1f-8c66-8b3e5403fd71>
2019-11-10 01:46:57 +0000

    More Python setup improvements

--- xci/source/info.apps/Manage-Outages/trunk/sbin/route_outages.sh
+++ xci/source/info.apps/Manage-Outages/trunk/sbin/route_outages.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-MY_ROOT=/soft/warehouse-apps-1.0/Manage-Outages/PROD
-WAREHOUSE_ROOT=/soft/warehouse-1.0/PROD
+APP_BASE=/soft/warehouse-apps-1.0/Manage-Outages
+APP_SOURCE=${APP_BASE}/PROD
+WAREHOUSE_SOURCE=/soft/warehouse-1.0/PROD
 
-PYTHON_BASE=/soft/python/python-3.7.5-base
+PYTHON_BASE=${APP_BASE}/`cat python/lib/python*/orig-prefix.txt`
 export LD_LIBRARY_PATH=${PYTHON_BASE}/lib
 
-PYTHON_ROOT=/soft/warehouse-apps-1.0/Manage-Outages/python
-source ${PYTHON_ROOT}/bin/activate
+PIPENV_BASE=${APP_BASE}/python
+source ${PIPENV_BASE}/bin/activate
 
 #export DJANGO_CONF=/Users/blau/info_services/info.warehouse/trunk/django_xsede_warehouse/xsede_warehouse/settings_localdev.conf
 #export PYTHONPATH=/Users/blau/info_services/info.warehouse/trunk/apps:/Users/blau/info_services/info.warehouse/trunk/django_xsede_warehouse
-export PYTHONPATH=${WAREHOUSE_ROOT}/django_xsede_warehouse
-export DJANGO_CONF=/soft/warehouse-apps-1.0/Manage-Outages/conf/django_xsede_warehouse.conf
+export PYTHONPATH=${WAREHOUSE_SOURCE}/django_xsede_warehouse
+export DJANGO_CONF=${APP_BASE}/conf/django_xsede_warehouse.conf
 export DJANGO_SETTINGS_MODULE=xsede_warehouse.settings
 
-python ${MY_ROOT}/sbin/route_outages.py
+${PIPENV_BASE}/bin/python ${APP_SOURCE}/sbin/route_outages.py
